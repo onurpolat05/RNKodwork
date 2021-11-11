f5ed95f2e1236b60333f2475f35c83f79a36965b
“Onur” <onurpolat711@gmail.com>
2021-11-11 14:45:41 +0300

    Feat: Hooks created

---
+++ src/Hooks/useFetch/useFetch.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export const useFetch = url => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  console.log(url);
+  const fetchData = async () => {
+    try {
+      const {data: responseData} = await axios.get(url);
+      setData(responseData);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return {error, data, loading};
+};
--- src/screens/Jops/jops.js
+++ src/screens/Jops/jops.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-
+import Config from 'react-native-config';
+import * as Request from '../../Hooks/useFetch/useFetch';
 export default function jops() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const {loading, data, error} = Request.useFetch(`${Config.API_URL}?page=0`);
+  const {results: jops} = data;
+  console.log(jops);
   return (
     <View>
       <Text>jops</Text>
